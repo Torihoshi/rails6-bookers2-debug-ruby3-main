a6c4d5cd7fc814b91656b3ad2345856097ec839b
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-07-01 04:53:08 +0000

    [Start]応用課題4 モデル作成 migration前まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,12 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
+  has_many :relationship
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-  
-  
+
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
