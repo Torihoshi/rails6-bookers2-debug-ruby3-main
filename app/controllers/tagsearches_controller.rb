fadadd684babcc2a576e7090d99ff02422a7e484
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-07-04 14:19:23 +0000

    [Add]tag表示

--- app/controllers/tagsearches_controller.rb
+++ app/controllers/tagsearches_controller.rb
@@ -4,7 +4,7 @@ class TagsearchesController < ApplicationController
     @model = Book  #search機能とは関係なし
     @word = params[:content]
     @books = Book.where("category LIKE?","%#{@word}%") #LIKE句は、主に曖昧検索を行う場合に使用するクエリ
-    render "searches/tagsearch"
+    render "tagsearches/tagsearch"
   end
 
 
